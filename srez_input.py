d4cd48fef9e4630069a2354ef9f5bccdad234ce0
gongenhao <gongenhao@gmail.com>
2017-05-07 23:01:27 -0700

    1) support specify non-square size 2) support specific sampling_pattern

--- srez_input.py
+++ srez_input.py
@@ -3,104 +3,9 @@ import numpy as np
 
 FLAGS = tf.app.flags.FLAGS
 
-def setup_inputs(sess, filenames, image_size=None, capacity_factor=3):
-
-    if image_size is None:
-        image_size = FLAGS.sample_size
-    
-    # Read each JPEG file
-    reader = tf.WholeFileReader()
-    filename_queue = tf.train.string_input_producer(filenames)
-    key, value = reader.read(filename_queue)
-    channels = 3
-    image = tf.image.decode_jpeg(value, channels=channels, name="dataset_image")
-    image.set_shape([None, None, channels])
-
-    # Crop and other random augmentations
-    image = tf.image.random_flip_left_right(image)
-    image = tf.image.random_saturation(image, .95, 1.05)
-    image = tf.image.random_brightness(image, .05)
-    image = tf.image.random_contrast(image, .95, 1.05)
-
-    wiggle = 8
-    off_x, off_y = 25-wiggle, 60-wiggle
-    crop_size = FLAGS.sample_size
-    crop_size_plus = crop_size + 2*wiggle
-    image = tf.image.crop_to_bounding_box(image, off_y, off_x, crop_size_plus, crop_size_plus)
-    image = tf.random_crop(image, [crop_size, crop_size, 3])
-
-    image = tf.reshape(image, [1, crop_size, crop_size, 3])
-    image = tf.cast(image, tf.float32)/255.0 # from 0~1
-
-    if crop_size != image_size:
-        image = tf.image.resize_area(image, [image_size, image_size])
- 
-    # The feature is simply a Kx downscaled version
-    K = 4
-    downsampled = tf.image.resize_area(image, [image_size//K, image_size//K])
-
-    feature = tf.reshape(downsampled, [image_size//K, image_size//K, 3])
-    label   = tf.reshape(image,       [image_size,   image_size,     3])
-
-    # Using asynchronous queues
-    features, labels = tf.train.batch([feature, label],
-                                      batch_size=FLAGS.batch_size,
-                                      num_threads=4,
-                                      capacity = capacity_factor*FLAGS.batch_size,
-                                      name='labels_and_features')
-
-    tf.train.start_queue_runners(sess=sess)
-      
-    return features, labels
-
-def setup_inputs_two_sources(sess, filenames_input, filenames_output, image_size=None, capacity_factor=3):
-
-    if image_size is None:
-        image_size = FLAGS.sample_size
-    
-    # Read each JPEG file
-    reader_input = tf.WholeFileReader()
-    filename_queue_input = tf.train.string_input_producer(filenames_input)
-    key, value_input = reader_input.read(filename_queue_input)
-    channels = 3
-    image_input = tf.image.decode_jpeg(value_input, channels=channels, name="input_image")
-    image_input.set_shape([None, None, channels])
-
-    # read output
-    reader_output = tf.WholeFileReader()
-    filename_queue_output = tf.train.string_input_producer(filenames_output)
-    key, value_output = reader_output.read(filename_queue_output)
-    channels = 3
-    image_output = tf.image.decode_jpeg(value_output, channels=channels, name="output_image")
-    image_output.set_shape([None, None, channels])
-
-    # cast
-    image_input = tf.cast(image_input, tf.float32)/255.0
-    image_output = tf.cast(image_output, tf.float32)/255.0
-
-    # do undersampling here
-
-    # take channel0 real part, channel1 imag part    
-    image_input = image_input[:,:,:2]
-    image_output = image_output[:,:,0]
-
-    # The feature is simply a Kx downscaled version
-    feature = tf.reshape(image_input, [image_size, image_size, 2])
-    label   = tf.reshape(image_output, [image_size,   image_size,     1])
-
-    # Using asynchronous queues
-    features, labels = tf.train.batch([feature, label],
-                                      batch_size=FLAGS.batch_size,
-                                      num_threads=4,
-                                      capacity = capacity_factor*FLAGS.batch_size,
-                                      name='labels_and_features')
-
-    tf.train.start_queue_runners(sess=sess)
-      
-    return features, labels
-
 # generate mask based on alpha
-def generate_mask_alpha(size=[128,128], r_factor_designed=5.0, r_alpha=3, acs=3, seed=0, axis_undersample=1, mute=0):
+def generate_mask_alpha(size=[128,128], r_factor_designed=5.0, r_alpha=3, axis_undersample=1,
+                        acs=3, seed=0, mute=0):
     # init
     mask = np.zeros(size)
     np.random.seed(seed)
@@ -138,22 +43,42 @@ def generate_mask_alpha(size=[128,128], r_factor_designed=5.0, r_alpha=3, acs=3,
     return mask, r_factor
 
 
+# generate mask based on .mat mask
+def generate_mask_mat(mask=[], mute=0):
+    # shift
+    mask = np.fft.ifftshift(mask)
+    # compute reduction
+    r_factor = len(mask.flatten())/sum(mask.flatten())
+    if not mute:
+        print('load mask size of {1} for R-factor={0:.4f}'.format(r_factor, mask.shape))
+    return mask, r_factor
+
+
 def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size=None, 
-                             axis_undersample=1, capacity_factor=3, r_factor=4, r_alpha=0, undersample_mask=None):
+                             axis_undersample=1, capacity_factor=3, r_factor=4, r_alpha=0, sampling_mask=None):
 
     # generate default mask
-    if undersample_mask is None:
-        DEFAULT_MASK, _ = generate_mask_alpha([image_size,image_size], # kspace size
+    if sampling_mask is None:
+        DEFAULT_MASK, _ = generate_mask_alpha(image_size, # kspace size
                                               r_factor_designed=r_factor, 
                                               r_alpha=r_alpha, 
                                               axis_undersample=axis_undersample)
     else:
-        DEFAULT_MASK = undersample_mask
+        # get input mask
+        DEFAULT_MASK, _ = generate_mask_mat(sampling_mask)
+
+    # convert to complex tf tensor
     DEFAULT_MAKS_TF = tf.cast(tf.constant(DEFAULT_MASK), tf.float32)
     DEFAULT_MAKS_TF_c = tf.cast(DEFAULT_MAKS_TF, tf.complex64)
 
+    # image size
     if image_size is None:
-        image_size = FLAGS.sample_size
+        # image_size
+        if FLAGS.sample_size_y>0:
+            image_size = [FLAGS.sample_size, FLAGS.sample_size_y]
+        else:
+            image_size = [FLAGS.sample_size, FLAGS.sample_size]
+
     
     # Read each JPEG file
     reader_input = tf.WholeFileReader()
@@ -163,30 +88,28 @@ def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size
     image_input = tf.image.decode_jpeg(value_input, channels=channels, name="input_image")
     image_input.set_shape([None, None, channels])
 
-    # cast
+    # cast image to float in 0~1
     image_input = tf.cast(image_input, tf.float32)/255.0
 
-    # take channel0 real part, channel1 imag part    
+    # use the last channel (B) for input and output, assume image is in gray-scale
     image_output = image_input[:,:,-1]
     image_input = image_input[:,:,-1]
-    
 
-    # undersample here
+    # apply undersampling mask
     kspace_input = tf.fft2d(tf.cast(image_input,tf.complex64))
     kspace_zpad = kspace_input * DEFAULT_MAKS_TF_c
+    # zpad undersampled image for input
     image_zpad = tf.ifft2d(kspace_zpad)
     image_zpad_real = tf.real(image_zpad)
-    image_zpad_real = tf.reshape(image_zpad_real, [image_size, image_size, 1])
-    # image_zpad_real.set_shape([image_size, image_size, 1])
+    image_zpad_real = tf.reshape(image_zpad_real, [image_size[0], image_size[1], 1])
     image_zpad_imag = tf.imag(image_zpad)
-    image_zpad_imag = tf.reshape(image_zpad_imag, [image_size, image_size, 1])    
-    # image_zpad_imag.set_shape([image_size, image_size, 1])
+    image_zpad_imag = tf.reshape(image_zpad_imag, [image_size[0], image_size[1], 1])    
+    # concat to input, 2 channel for real and imag value
     image_zpad_concat = tf.concat(axis=2, values=[image_zpad_real, image_zpad_imag])
 
-
-    # The feature is simply a Kx downscaled version
-    feature = tf.reshape(image_zpad_concat, [image_size, image_size, 2])
-    label   = tf.reshape(image_output, [image_size,   image_size,     1])
+    # The feature is zpad image with 2 channel, label is the ground-truth real-valued image
+    feature = tf.reshape(image_zpad_concat, [image_size[0], image_size[1], 2])
+    label   = tf.reshape(image_output, [image_size[0], image_size[1], 1])
 
     # Using asynchronous queues
     features, labels = tf.train.batch([feature, label],
