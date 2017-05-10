815623ecdd0cfee12ec03aafbf3a7031c0d73021
gongenhao <gongenhao@gmail.com>
2017-05-09 22:58:32 -0700

    update loss err log

--- srez_train.py
+++ srez_train.py
@@ -157,9 +157,9 @@ def train_model(train_data, num_sample_train=1984, num_sample_test=116):
         if batch % 10 == 0:
             # Show we are alive
             elapsed = int(time.time() - start_time)/60
-            err_log = 'Progress[{0:3d}%], ETA[{1:4d}m], Batch [{2:4d}], G_DC_Loss[{3:3.3f}], G_LS_Loss[{4:3.3f}], D_Real_Loss[{5:3.3f}], D_Fake_Loss[{6:3.3f}]'.format(
+            err_log = 'Progress[{0:3d}%], ETA[{1:4d}m], Batch [{2:4d}], G_Loss[{3:3.3f}], G_DC_Loss[{4:3.3f}], G_LS_Loss[{5:3.3f}], D_Real_Loss[{6:3.3f}], D_Fake_Loss[{7:3.3f}]'.format(
                     int(100*elapsed/FLAGS.train_time), FLAGS.train_time - elapsed, batch, 
-                    gene_dc_loss, gene_ls_loss, disc_real_loss, disc_fake_loss)
+                    gen_loss, gene_dc_loss, gene_ls_loss, disc_real_loss, disc_fake_loss)
             print(err_log)
 
             # Finished?            
