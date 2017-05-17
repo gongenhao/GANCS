ae53d2e5b63e9178bdaecce3bb78597af3f4903f
gongenhao <gongenhao@gmail.com>
2017-05-17 05:39:38 -0700

    skip more gene layers

--- srez_train.py
+++ srez_train.py
@@ -206,8 +206,9 @@ def train_model(train_data, num_sample_train=1984, num_sample_test=116):
                               'train_loss':err_loss,
                               'gene_loss':list_gene_losses,
                               'inference_time':inference_time,
-                              'gene_layers':[x.tolist() for x in gene_layers], 
+                              'gene_layers':[x.tolist() for x in gene_layers if x.shape()[-1]<10], 
                               'disc_layers':[x.tolist() for x in disc_layers]}                
+                # gene layers are too large
                 if index_batch_test>0:
                     gene_param['gene_layers']=[]
                 _summarize_progress(td, test_feature, test_label, gene_output, batch, 
